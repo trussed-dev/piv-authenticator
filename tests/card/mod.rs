f3324bdd81137c2e5842fa7d0a19adc350955c20
Sosthène Guédon <sosthene@nitrokey.com>
2022-11-21 17:49:26 +0100

    Add basic pivy test

---
+++ tests/card/mod.rs
@@ -0,0 +1,37 @@
+use piv_authenticator::{vpicc::VirtualCard, Authenticator};
+
+use std::{sync::mpsc, thread::sleep, time::Duration};
+use stoppable_thread::spawn;
+
+pub fn with_vsc<F: FnOnce() -> R, R>(f: F) -> R {
+    let mut vpicc = vpicc::connect().expect("failed to connect to vpcd");
+
+    let (tx, rx) = mpsc::channel();
+    let handle = spawn(move |stopped| {
+        trussed::virt::with_ram_client("opcard", |client| {
+            let card = Authenticator::new(client);
+            let mut virtual_card = VirtualCard::new(card);
+            let mut result = Ok(());
+            while !stopped.get() && result.is_ok() {
+                result = vpicc.poll(&mut virtual_card);
+                if result.is_ok() {
+                    tx.send(()).expect("failed to send message");
+                }
+            }
+            result
+        })
+    });
+
+    rx.recv().expect("failed to read message");
+
+    sleep(Duration::from_millis(200));
+
+    let result = f();
+
+    handle
+        .stop()
+        .join()
+        .expect("failed to join vpicc thread")
+        .expect("failed to run virtual smartcard");
+    result
+}
---
+++ tests/pivy.rs
@@ -0,0 +1,21 @@
+#![cfg(feature = "virtual")]
+
+mod card;
+
+use card::with_vsc;
+
+use expectrl::{spawn, Eof, Regex, WaitStatus};
+
+#[test]
+fn list() {
+    with_vsc(|| {
+        let mut p = spawn("pivy-tool list").unwrap();
+        p.check(Regex("card: [0-9A-Z]*")).unwrap();
+        p.check("device: Virtual PCD 00 00").unwrap();
+        p.check("chuid: ok").unwrap();
+        p.check(Regex("guid: [0-9A-Z]*")).unwrap();
+        p.check("algos: 3DES AES256 ECCP256 (null) (null)").unwrap();
+        p.check(Eof).unwrap();
+        assert_eq!(p.wait().unwrap(), WaitStatus::Exited(p.pid(), 0));
+    });
+}

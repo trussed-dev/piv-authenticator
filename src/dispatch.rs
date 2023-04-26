1c1e9a30b4caa992fb0703ce0023e81d44eb7773
Sosthène Guédon <sosthene@nitrokey.com>
2023-04-26 11:23:12 +0200

    Migrate to upstream trussed and trussed-staging for streaming writes

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -4,13 +4,11 @@
 use crate::{reply::Reply, Authenticator, /*constants::PIV_AID,*/ Result};
 
 use apdu_dispatch::{app::App, command, response, Command};
-use trussed::client;
-use trussed_auth::AuthClient;
 
 #[cfg(feature = "apdu-dispatch")]
 impl<T> App<{ command::SIZE }, { response::SIZE }> for Authenticator<T>
 where
-    T: client::Client + AuthClient + client::Ed255 + client::Tdes,
+    T: crate::Client,
 {
     fn select(&mut self, _apdu: &Command, reply: &mut response::Data) -> Result {
         self.select(Reply(reply))

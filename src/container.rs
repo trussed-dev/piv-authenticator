03c27640b064f0a582157df5e2ca9c6cd791be13
Sosthène Guédon <sosthene@nitrokey.com>
2024-08-26 11:57:41 +0200

    Fix key history object

--- src/container.rs
+++ src/container.rs
@@ -76,6 +76,15 @@ macro_rules! enum_subset {
                 }
             }
         }
+
+        impl $name {
+            #[allow(unused)]
+            pub(crate) fn all() -> &'static [Self] {
+                &[
+                    $(Self::$var,)*
+                ]
+            }
+        }
     }
 }
 

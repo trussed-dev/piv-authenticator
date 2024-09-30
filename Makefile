82aed8b06b881f8fb26eb5c59f647184c433e715
Sosthène Guédon <sosthene@nitrokey.com>
2024-09-30 16:00:07 +0200

    Enable test suite to run on a hardware device

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 .NOTPARALLEL:
 
+-include variables.mk
+
 export RUST_LOG ?= info,cargo_tarpaulin=off
 TEST_FEATURES ?=vpicc,pivy-tests,opensc-tests,rsa
+export PIV_DANGEROUS_TEST_CARD_READER ?= Virtual PCD 00 00
+export PIV_DANGEROUS_TEST_CARD_PIV_SERIAL ?= 04 B2 BB FB 54 40 4A E3 9B B8 6A E3 CA 82 9C 24
 
 .PHONY: build-cortex-m4
 build-cortex-m4:
@@ -10,6 +14,10 @@ build-cortex-m4:
 .PHONY: test
 test:
 	cargo test --features $(TEST_FEATURES)
+
+.PHONY: dangerous-test-real-card
+dangerous-test-real-card:
+	cargo test --features $(TEST_FEATURES),dangerous-test-real-card
 
 .PHONY: check
 check:
--- tests/card/mod.rs
+++ tests/card/mod.rs
@@ -3,13 +3,18 @@ use piv_authenticator::{virt::with_ram_client, vpicc::VpiccCard, Authenticator, 
 use std::{sync::mpsc, thread::sleep, time::Duration};
 use stoppable_thread::spawn;
 
+use std::panic::{catch_unwind, resume_unwind, UnwindSafe};
+use std::process::Command;
 use std::sync::Mutex;
 
 static VSC_MUTEX: Mutex<()> = Mutex::new(());
 
+#[cfg_attr(feature = "dangerous-test-real-card", expect(unused))]
 pub const WITH_UUID: Options = Options::new().uuid(Some([0; 16]));
+#[cfg_attr(feature = "dangerous-test-real-card", expect(unused))]
 pub const WITHOUT_UUID: Options = Options::new();
 
+#[cfg_attr(feature = "dangerous-test-real-card", expect(unused))]
 pub fn with_vsc<F: FnOnce() -> R, R>(options: Options, f: F) -> R {
     let Ok(_lock) = VSC_MUTEX.lock() else {
         panic!("Some other test failed, this test is therefore ignored")
@@ -45,4 +50,33 @@ pub fn with_vsc<F: FnOnce() -> R, R>(options: Options, f: F) -> R {
         .expect("failed to join vpicc thread")
         .expect("failed to run vpicc smartcard");
     result
+}
+
+#[cfg_attr(not(feature = "dangerous-test-real-card"), expect(unused))]
+pub fn with_lock_and_reset<F: UnwindSafe + FnOnce() -> R, R: UnwindSafe>(f: F) {
+    let lock = VSC_MUTEX.lock();
+    let res = catch_unwind(f);
+    let output = Command::new("piv-tool")
+        .args([
+            "-s",
+            "00:20:00:80:08:0102030405060708",
+            "-s",
+            "00:20:00:80:08:0102030405060708",
+            "-s",
+            "00:20:00:80:08:0102030405060708", //Locked pin
+            "-s",
+            "00:FB:00:00",
+        ])
+        .output()
+        .unwrap();
+    if let Err(err) = res {
+        resume_unwind(err)
+    }
+
+    println!("Out: {}", String::from_utf8_lossy(&output.stdout));
+    println!("Err: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
+
+    drop(lock);
 }

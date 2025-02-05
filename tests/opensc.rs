a9dba5932b4d391506dc46a996374fcca99a2f58
Sosthène Guédon <sosthene@nitrokey.com>
2025-02-05 16:53:35 +0100

    Add OpenSC tests with bad keys

--- tests/opensc.rs
+++ tests/opensc.rs
@@ -75,13 +75,68 @@ fn admin_card() {
             .args(["-A", "A:9B:03"]);
         let mut p = expectrl::session::Session::spawn(command).unwrap();
         p.set_expect_timeout(EXPECT_TIMEOUT);
-        p.expect("Using reader with a card: Virtual PCD 00 00")
+        p.expect(format!("Using reader with a card: {CARD}"))
             .unwrap();
-        // p.expect("Personal Identity Verification Card").unwrap();
         p.expect(Eof).unwrap();
         assert_eq!(
             p.get_process().wait().unwrap(),
             WaitStatus::Exited(p.get_process().pid(), 0)
+        );
+    };
+    cfg_if! {
+        if #[cfg(not(feature = "dangerous-test-real-card"))]{
+            with_vsc(WITH_UUID, test);
+            with_vsc(WITHOUT_UUID, test);
+        } else {
+            with_lock_and_reset(test)
+        }
+    }
+}
+
+#[test_log::test]
+fn admin_mutual_bad_key() {
+    let test = || {
+        let mut command = Command::new("piv-tool");
+        command
+            .env("PIV_EXT_AUTH_KEY", "tests/bad_admin_key")
+            .args(["-A", "M:9B:03"]);
+        let mut p = expectrl::session::Session::spawn(command).unwrap();
+        p.set_expect_timeout(EXPECT_TIMEOUT);
+        p.expect(format!("Using reader with a card: {CARD}"))
+            .unwrap();
+        p.expect(" admin_mode failed -1205").unwrap();
+        p.expect(Eof).unwrap();
+        assert_eq!(
+            p.get_process().wait().unwrap(),
+            WaitStatus::Exited(p.get_process().pid(), 75)
+        );
+    };
+    cfg_if! {
+        if #[cfg(not(feature = "dangerous-test-real-card"))]{
+            with_vsc(WITH_UUID, test);
+            with_vsc(WITHOUT_UUID, test);
+        } else {
+            with_lock_and_reset(test)
+        }
+    }
+}
+
+#[test_log::test]
+fn admin_card_bad_key() {
+    let test = || {
+        let mut command = Command::new("piv-tool");
+        command
+            .env("PIV_EXT_AUTH_KEY", "tests/bad_admin_key")
+            .args(["-A", "A:9B:03"]);
+        let mut p = expectrl::session::Session::spawn(command).unwrap();
+        p.set_expect_timeout(EXPECT_TIMEOUT);
+        p.expect(format!("Using reader with a card: {CARD}"))
+            .unwrap();
+        p.expect(" admin_mode failed -1205").unwrap();
+        p.expect(Eof).unwrap();
+        assert_eq!(
+            p.get_process().wait().unwrap(),
+            WaitStatus::Exited(p.get_process().pid(), 75)
         );
     };
     cfg_if! {
@@ -102,7 +157,8 @@ fn generate_key() {
             .env("PIV_EXT_AUTH_KEY", "tests/default_admin_key")
             .args(["-A", "M:9B:03", "-G", "9A:11"]);
         let mut p = expectrl::session::Session::spawn(command).unwrap();
-        p.expect("Using reader with a card: Virtual PCD 00 00")
+        p.set_expect_timeout(EXPECT_TIMEOUT);
+        p.expect(format!("Using reader with a card: {CARD}"))
             .unwrap();
         p.expect(Eof).unwrap();
         // Non zero exit code?
@@ -126,7 +182,8 @@ fn generate_key() {
             .env("PIV_EXT_AUTH_KEY", "tests/default_admin_key")
             .args(["-A", "M:9B:03", "-G", "9A:07"]);
         let mut p = expectrl::session::Session::spawn(command).unwrap();
-        p.expect("Using reader with a card: Virtual PCD 00 00")
+        p.set_expect_timeout(EXPECT_TIMEOUT);
+        p.expect(format!("Using reader with a card: {CARD}"))
             .unwrap();
         p.expect(Eof).unwrap();
         // Non zero exit code?
